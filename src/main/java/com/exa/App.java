f47a9df08b6b78c843db68863adab31086d870ef
karthik murali <karthikmurali123@gmail.com>
2025-07-06 18:12:28 +0530

    build1

---
+++ src/main/java/com/exa/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven!");
+    }
+
+    // Additional methods for core functionality can be added here
+}
